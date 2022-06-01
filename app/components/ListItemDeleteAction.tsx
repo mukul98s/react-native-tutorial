be5d0b1ba5d9654c6791e4c4f9210136b09cabe8
Mukul Sharma <mymukul112@gmail.com>
2022-06-01 12:07:47 +0530

    Utility component for swipeable components

---
+++ app/components/ListItemDeleteAction.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableWithoutFeedbackProps,
+} from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+
+import colors from "../config/colors";
+
+function ListItemDeleteAction({
+  onPress,
+}: {
+  onPress: TouchableWithoutFeedbackProps["onPress"];
+}) {
+  return (
+    <TouchableWithoutFeedback onPress={onPress}>
+      <View style={styles.container}>
+        {/* @ts-ignore */}
+        <MaterialCommunityIcons
+          name="trash-can"
+          size={35}
+          color={colors.white}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.danger,
+    width: 70,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default ListItemDeleteAction;
