477d15ee4939b7920af2f4777c177effd2e0e24a
Mukul Sharma <mymukul112@gmail.com>
2022-06-01 12:08:49 +0530

    global types

---
+++ app/types/global.d.ts
@@ -0,0 +1,5 @@
+import colors from "../config/colors";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+
+type IconType = keyof typeof MaterialCommunityIcons.glyphMap;
+type Color = keyof typeof colors;
