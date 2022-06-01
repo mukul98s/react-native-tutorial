4d0fedcc7966d3ade7599f7ad20280bc40979f76
Mukul Sharma <mymukul112@gmail.com>
2022-06-01 17:45:32 +0530

    replace styles with default styles

--- app/components/AppText.tsx
+++ app/components/AppText.tsx
@@ -1,22 +1,16 @@
 import * as React from "react";
-import {Text, TextProps, StyleSheet, Platform} from "react-native";
-import colors from "../config/colors";
+import {Text, TextProps} from "react-native";
+
+import defaultStyles from "../config/styles";
 
 interface Props extends TextProps {}
 
 const AppText: React.FC<Props> = ({children, style, ...rest}) => {
   return (
-    <Text style={[styles.text, style]} {...rest}>
+    <Text style={[defaultStyles.text, style]} {...rest}>
       {children}
     </Text>
   );
 };
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
-  },
-});
 
 export default AppText;
