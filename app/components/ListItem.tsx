7826b08987dc092fa389ab69afe0820e6cdccb07
Mukul Sharma <mymukul112@gmail.com>
2022-06-01 12:07:28 +0530

    Update props - add swipte feature

--- app/components/ListItem.tsx
+++ app/components/ListItem.tsx
@@ -9,25 +9,44 @@ import {
 } from "react-native";
 import colors from "../config/colors";
 import AppText from "./AppText";
+import Swipeable, {
+  SwipeableProps,
+} from "react-native-gesture-handler/Swipeable";
 
 interface Props {
   title: string;
-  subtitle: string;
-  image: ImageSourcePropType;
-  onPress: TouchableHighlightProps["onPress"];
+  subtitle?: string;
+  image?: ImageSourcePropType;
+  onPress?: TouchableHighlightProps["onPress"];
+  renderRightAction?: SwipeableProps["renderRightActions"];
+  IconComponent?: React.ReactNode;
 }
 
-const ListItem: React.FC<Props> = ({title, subtitle, image, onPress}) => {
+const ListItem: React.FC<Props> = ({
+  title,
+  subtitle,
+  image,
+  onPress,
+  renderRightAction,
+  IconComponent,
+}) => {
   return (
-    <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
-      <View style={styles.container}>
-        <Image style={styles.image} source={image} />
-        <View>
-          <AppText style={styles.title}>{title}</AppText>
-          <AppText style={styles.subtitle}>{subtitle}</AppText>
+    <Swipeable renderRightActions={renderRightAction}>
+      <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
+        <View style={styles.container}>
+          <>
+            {IconComponent}
+            {image && <Image style={styles.image} source={image} />}
+            <View style={styles.detailsContainer}>
+              <AppText style={styles.title}>{title}</AppText>
+              {subtitle && (
+                <AppText style={styles.subtitle}>{subtitle}</AppText>
+              )}
+            </View>
+          </>
         </View>
-      </View>
-    </TouchableHighlight>
+      </TouchableHighlight>
+    </Swipeable>
   );
 };
 
@@ -36,18 +55,22 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     padding: 10,
+    backgroundColor: colors.white,
   },
   image: {
     width: 70,
     height: 70,
     borderRadius: 35,
-    marginRight: 10,
   },
   title: {
     fontWeight: "500",
   },
   subtitle: {
     color: colors.medium,
+  },
+  detailsContainer: {
+    marginLeft: 10,
+    justifyContent: "center",
   },
 });
 
