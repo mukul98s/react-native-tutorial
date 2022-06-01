5c4ba56ca6e8695a70a0d078ee1ce0c2497faa3f
Mukul Sharma <mymukul112@gmail.com>
2022-06-01 17:46:10 +0530

    text input component

---
+++ app/components/AppTextInput.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import {View, TextInput, TextInputProps, StyleSheet} from "react-native";
+
+import {IconType} from "../types/global";
+import colors from "../config/colors";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import defaultStyles from "../config/styles";
+
+interface Props extends TextInputProps {
+  icon?: IconType;
+}
+
+const AppTextInput: React.FC<Props> = ({icon, ...rest}) => {
+  return (
+    <View style={styles.container}>
+      {icon && (
+        // @ts-ignore
+        <MaterialCommunityIcons
+          style={styles.icon}
+          name={icon}
+          size={20}
+          color={colors.medium}
+        />
+      )}
+      <TextInput style={defaultStyles.text} {...rest} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+    alignItems: "center",
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
+
+export default AppTextInput;
