35e9289e7b8137b89c177324ecd2cbf4ab519de2
Mukul Sharma <mymukul112@gmail.com>
2022-06-02 17:27:56 +0530

    add types for Picker Component

--- app/types/global.d.ts
+++ app/types/global.d.ts
@@ -1,5 +1,18 @@
 import colors from "../config/colors";
 import {MaterialCommunityIcons} from "@expo/vector-icons";
+import {TouchableOpacityProps} from "react-native";
 
 type IconType = keyof typeof MaterialCommunityIcons.glyphMap;
 type Color = keyof typeof colors;
+
+type PicketItemComponentProps = {
+  item: PickerItemType;
+  onPress: TouchableOpacityProps["onPress"];
+};
+
+type PickerItemType = {
+  backgroundColor?: Color;
+  icon?: IconType;
+  label: string;
+  value: string | number;
+};
